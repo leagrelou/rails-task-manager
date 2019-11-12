7e45d1eccacaab15d0eba250b1087dd66173f449
Lea Grelou <leagrelou@gmail.com>
2019-11-12 14:13:37 -0500

    ongoing exercice 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # VERB PATH, to: 'CONTROLLER#ACTION'
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show'
 end
